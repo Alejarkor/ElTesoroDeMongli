3bc4fc7d382c286dd58e1de4746aee0653c5674a
Alejandro langarica <alelangaapa@gmail.com>
2026-05-16 21:23:11 +0200

    refactor: add UnityWebRequest awaiter

---
+++ Assets/_Project/Scripts/API/UnityWebRequestAwaiter.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine.Networking;
+
+namespace ElTesoroDeMongli.API
+{
+    public static class UnityWebRequestAwaiterExtensions
+    {
+        public static UnityWebRequestAsyncOperationAwaiter GetAwaiter(this UnityWebRequestAsyncOperation asyncOperation)
+        {
+            return new UnityWebRequestAsyncOperationAwaiter(asyncOperation);
+        }
+    }
+
+    public class UnityWebRequestAsyncOperationAwaiter : INotifyCompletion
+    {
+        private readonly UnityWebRequestAsyncOperation asyncOperation;
+        private Action continuation;
+
+        public UnityWebRequestAsyncOperationAwaiter(UnityWebRequestAsyncOperation asyncOperation)
+        {
+            this.asyncOperation = asyncOperation;
+            asyncOperation.completed += OnCompleted;
+        }
+
+        public bool IsCompleted => asyncOperation.isDone;
+
+        public void GetResult()
+        {
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            this.continuation = continuation;
+        }
+
+        private void OnCompleted(AsyncOperation operation)
+        {
+            continuation?.Invoke();
+        }
+    }
+}
